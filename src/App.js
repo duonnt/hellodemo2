bc341e1c195f6085481586958b01857efc6876cc
duonnt <jsjygabriel@hotmail.com>
2021-04-30 16:12:31 +0800

    update

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+export default class App extends React.Component{
+    constructor(){
+        super();
+        this.state = {
+            counter : 0
+        }
+    }
+    render(){
+        return (
+            <div>
+                <h2>当前计数：{this.state.counter}</h2>
+                <button>+</button>
+                <button>-</button>
+            </div>
+        )
+    }
+}
---
+++ src/utils.js
@@ -0,0 +1,3 @@
+export function hellosum (num1, num2){
+    return num1 + num2 ;
+}
